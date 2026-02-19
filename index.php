1896d3ebbdc69a572ac3aaf511951d529f7dde77
Jesse Kretschmer <jesse@krets.com>
2026-02-19 15:37:29 +0100

    using a "fixed app layout" maybe

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once 'config.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rental Property Map Generator</title>
-    <link rel="stylesheet" href="style.css?v=20260219_2">
+    <link rel="stylesheet" href="style.css?v=20260219_3">
     <style>
         /* Fail-safe mobile trigger */
         @media (max-width: 1100px) {
